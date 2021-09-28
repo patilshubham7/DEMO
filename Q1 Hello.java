8a77f5560cff117770a828853bb88c592592528c
patilshubham7 <91456719+patilshubham7@users.noreply.github.com>
2021-09-28 12:19:08 +0530

    Two new files uploaded.

---
+++ Q1 Hello.java
@@ -0,0 +1,18 @@
+package mathclass;
+
+public class conditional {
+
+	public static void main(String[] args)
+	{
+			System.out.println("HELLO");
+			System.out.println("SHUBHAM");
+		
+			
+			
+		
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Q2 Swap.java
@@ -0,0 +1,19 @@
+package mathclass;
+
+public class a 
+{
+
+	public static void main(String[] args)
+       {
+		int a=10;
+		int b=20;
+		int temp = a;
+		System.out.println("Before swap a and b " +a+" "+b);
+		a=b;
+		b=temp;
+		System.out.println("After swap a and b " +a+" "+b);
+		
+
+	}
+
+}
