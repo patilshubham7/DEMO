92f26760d4e0328a3cd7933ab9d44785d9c01a0c
patilshubham7 <91456719+patilshubham7@users.noreply.github.com>
2021-09-28 12:30:04 +0530

    3rd program file

---
+++ Q4 EvenOddNumber.java
@@ -0,0 +1,20 @@
+package mathclass;
+
+public class c 
+{
+
+	public static void main(String[] args) 
+       {
+		int num =11;
+		if (num % 2==0)
+		  {
+		   System.out.println("Number is Even");
+		  }
+		  else
+		  {
+		   System.out.println("Number is Odd ");
+		  }
+
+	}
+
+}
